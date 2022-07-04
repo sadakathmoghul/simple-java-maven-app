87495cf55e1b118b3d351ad4267a395dad93149d
sadakathmoghul <103745596+sadakathmoghul@users.noreply.github.com>
2022-07-04 15:42:55 +0530

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -42,7 +42,7 @@ pipeline {
 						ls -lrt						
 						sudo cp -rv target/*.jar /home/ec2-user/apache-tomcat-7.0.32/webapps
 						#sudo cp -rv $HOME/jenkins/SampleWebApp.war /usr/share/tomcat/webapps
-						
+						exit 1
 					'''
 					build job: "../Team2/FirstJob", wait: false
 				}
